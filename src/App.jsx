ce5069ed668f7d3b57f7157642cad73f8b3884fe
Andrii Rubtsov <andrii.rubtsov@gmail.com>
2023-02-22 01:41:18 +0200

    Update 5

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App text-cyan-400">
-      <h1 class="text-4xl text-center">Coming soon. Really. 4</h1>
+      <h1 class="text-4xl text-center">Coming soon. Really. 5</h1>
       <p class="my-4 mx-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae dolor neque sunt, obcaecati quo iure voluptatum ab expedita earum aspernatur accusantium inventore accusamus odit consequuntur suscipit officia amet ut ipsam!</p>      
     </div>
   )
