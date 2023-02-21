9247a7a768f6e7b3b85a7395aa3806836f7e3980
Andrii Rubtsov <andrii.rubtsov@gmail.com>
2023-02-21 20:37:45 +0200

    Init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App text-cyan-400">
+      <h1 class="text-4xl text-center">Vite + React  1212</h1>
+      <p class="my-4 mx-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae dolor neque sunt, obcaecati quo iure voluptatum ab expedita earum aspernatur accusantium inventore accusamus odit consequuntur suscipit officia amet ut ipsam!</p>      
+    </div>
+  )
+}
+
+export default App
